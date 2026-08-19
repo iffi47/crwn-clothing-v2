293c22ee714f7623db46023e732cd0c4f878b42f
iffi47 <iftikharlion958@gmail.com>
2026-08-19 18:05:32 +0500

    Add Sass support and refactor App component to use categories array

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
+const categories = [
+ {
+  id: 1,
+  title: "Hats",
+ },
+ {
+  id: 2,
+  title: "Jackets",
+ },
+ {
+  id: 3,
+  title: "Sneakers",
+ },
+ {
+  id: 4,
+  title: "Men",
+ },
+ {
+  id: 5,
+  title: "Women",
+ },
+];
 const App = () => {
-  return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+ return (
+  <div className="categories-container">
+   {categories.map((cat) => (
+    <div
+     key={cat.id}
+     className="category-container">
+     {/* <img /> */}
+     <div className="category-body-container">
+      <h2>{cat.title}</h2>
+      <p>Shop Now</p>
+     </div>
     </div>
-  );
+   ))}
+  </div>
+ );
 };
 
 export default App;

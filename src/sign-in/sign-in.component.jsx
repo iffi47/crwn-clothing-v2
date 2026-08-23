e53f70ce8b1c0e8c2a15c3a8b47a7024d2e758e3
iffi47 <iftikharlion958@gmail.com>
2026-08-23 18:14:25 +0500

    refactor: fix env variables access method

--- src/sign-in/sign-in.component.jsx
+++ src/sign-in/sign-in.component.jsx
@@ -1,10 +1,19 @@
+import { log } from "firebase/firestore/pipelines";
+import { signinWithGooglePopup } from "../utlis/firebase.utils"
+
 export default function SignIn() {
+  const logGoogleUser = async () => {
+    const response = await signinWithGooglePopup();
+    console.log(response);
+
+  }
   return(
     <>
       <div>
         <h1>
           Sign In Page
         </h1>
+        <button onClick={logGoogleUser}>Sign in With Google</button>
       </div>
     </>
   )

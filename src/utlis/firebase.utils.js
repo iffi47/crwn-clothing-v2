6ee24570b82aab850c75c1cc7c8a373f18b82a32
iffi47 <iftikharlion958@gmail.com>
2026-08-23 16:55:56 +0500

    feat: replace firebase variables iinto .env file

--- src/utlis/firebase.utils.js
+++ src/utlis/firebase.utils.js
@@ -4,12 +4,12 @@ import { getAuth, signInWithRedirect, GoogleAuthProvider,signInWithPopup } from 
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBwGJ6-3MZtz2bJUPzKzlDipeALZIvR2FY",
-  authDomain: "crwn-clothing-db-7f14b.firebaseapp.com",
-  projectId: "crwn-clothing-db-7f14b",
-  storageBucket: "crwn-clothing-db-7f14b.firebasestorage.app",
-  messagingSenderId: "447323162573",
-  appId: "1:447323162573:web:880e012378ae9be8ae2684"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase

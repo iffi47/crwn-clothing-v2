a18677437890a3e0c2918877cae0679606e7ca3d
iffi47 <iftikharlion958@gmail.com>
2026-09-13 22:33:43 +0500

    feat: add routing link for category preview

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,15 +1,12 @@
 import "./category-preview.styles.scss";
 import ProductCard from "../product-card/product-card.component";
+import { Link } from "react-router-dom";
 
 export default function CategoryPreview({ title, products }) {
-  const handleTitleClick = () => {
-    console.log("Title");
-
-  }
   return (
     <>
       <div className="category-preview-container">
-        <h2><span onClick={handleTitleClick}>{title.toUpperCase()}</span></h2>
+        <h2><Link to={title}>{title.toUpperCase()}</Link></h2>
         <div className="preview">
           {
             products.filter((_, idx) => idx < 4).map((product) => (

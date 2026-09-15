75860cf6a1356007684f0594ba178ab3000f2434
iffi47 <iftikharlion958@gmail.com>
2026-09-15 12:54:19 +0500

    feat: add styled styling

--- src/components/cart-item/cart-item.styles.jsx
+++ src/components/cart-item/cart-item.styles.jsx
@@ -1,4 +1,6 @@
-.cart-item-container {
+import styled from "styled-components";
+
+export const CartItemContainer = styled.div`
   width: 100%;
   display: flex;
   height: 80px;
@@ -7,17 +9,17 @@
   img {
     width: 30%;
   }
-
-  .item-details {
-    width: 70%;
+`;
+export const ItemDetails=  styled.div`
+   width: 70%;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     justify-content: center;
     padding: 10px 20px;
+`;
 
-    .name {
-      font-size: 16px;
-    }
-  }
-}
+export const Name = styled.span`
+font-size: 16px;
+`
+
--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,20 +1,21 @@
-import "./category-preview.styles.scss";
+// import "./category-preview.styles.jsx";
+import { CategoryPreviewContainer, Title, Preview } from "./category-preview.styles";
 import ProductCard from "../product-card/product-card.component";
 import { Link } from "react-router-dom";
 
 export default function CategoryPreview({ title, products }) {
   return (
     <>
-      <div className="category-preview-container">
-        <h2><Link to={title}>{title.toUpperCase()}</Link></h2>
-        <div className="preview">
+      <CategoryPreviewContainer>
+        <Title><Link to={title}>{title.toUpperCase()}</Link></Title>
+        <Preview>
           {
             products.filter((_, idx) => idx < 4).map((product) => (
               <ProductCard key={product.id} product={product} />
             ))
           }
-        </div>
-      </div>
+        </Preview>
+      </CategoryPreviewContainer>
     </>
   )
 }
